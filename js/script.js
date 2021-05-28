c99ff14fea85ba04b62905f3de5bcdfd47e1ba32
Dawid Poznański <dawid.poznanski@outlook.com>
2021-05-28 12:26:48 +0200

    add computer moves, before was player moves

--- js/script.js
+++ js/script.js
@@ -1,3 +1,20 @@
+let randomNumber = Math.floor(Math.random() * 3 + 1);
+
+console.log('Wylosowana liczba to: ' + randomNumber);
+
+let computerMove = 'nieznany ruch';
+
+if(randomNumber == 1){
+  computerMove = 'kamień';
+}
+else if(playerInput == 2){
+    playerMove = 'papier'
+}
+else if(playerInput == 3){
+    playerMove = 'nożyce'
+}
+printMessage('Mój ruch to: ' + computerMove);
+
 let playerInput = prompt('Wybierz swój ruch! 1: kamień, 2: papier, 3: nożyce.');
 
 console.log('Gracz wpisał: ' + playerInput);
